20b4d598d95d0040d951a1502971ef20b97bdf05
Saimol JImenez <saimoljimenez@gmail.com>
2023-04-12 02:32:09 -0500

    interfaze del comienzo listo

--- src/conversor/interfaceUs.java
+++ src/conversor/interfaceUs.java
@@ -1,22 +1,42 @@
 package conversor;
 
+import javafx.scene.control.ComboBox;
+import javax.swing.ComboBoxEditor;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-
-public class interfaceUs extends JFrame {
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+public class interfaceUs extends JFrame implements ActionListener{
+    private JComboBox<String> comboBox;
     public static void main(String[] args) {
         interfaceUs frame = new interfaceUs();
         frame.setVisible(true);
     }
-    public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(this, "Haz hecho clic en el botón");
-    }
     public interfaceUs() {
-        setTitle("conversor");
-        setSize(400,600);
-        JButton button = new JButton("Haz clic");
-        setLocationRelativeTo(null);
-        add(button);
+        setTitle("Menu");
+        setSize(400,150);
+
+        String[] opciones = {"Conversor de moneda", "Conversor de Temperatura"};
+        comboBox = new JComboBox<>(opciones);
+        comboBox.addActionListener(this);
+        add(comboBox);
+        JLabel label = new JLabel("Selecciona una opción:");
+        JPanel panel = new JPanel();
+        panel.add(label);
+        panel.add(comboBox);
+
+        JLabel label1 = new JLabel("Selecciona una opción de convercion:");
+        JPanel panel1 = new JPanel();
+        JButton button1 = new JButton("Aceptat");
+        panel.add(label);
+        panel.add(comboBox);
+        panel.add(button1);
+        add(panel);
     }
 
+    public void actionPerformed(ActionEvent e) {
+        String seleccion = (String) comboBox.getSelectedItem();
+        JOptionPane.showMessageDialog(this, "Seleccionaste " + seleccion);
+    }
 }
