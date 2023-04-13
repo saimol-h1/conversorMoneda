fee4542e730db8da2bf14a6c9bd3d4a123650c9a
Saimol JImenez <saimoljimenez@gmail.com>
2023-04-12 23:03:00 -0500

    interface 2 compltada

--- src/conversor/interfaceUs.java
+++ src/conversor/interfaceUs.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 public class interfaceUs extends JFrame implements ActionListener{
     private JComboBox<String> comboBox;
+    private JFrame ventanaCorrespondiente;
     public static void main(String[] args) {
         interfaceUs frame = new interfaceUs();
         frame.setVisible(true);
@@ -28,7 +29,7 @@ public class interfaceUs extends JFrame implements ActionListener{
 
         JLabel label1 = new JLabel("Selecciona una opción de convercion:");
         JPanel panel1 = new JPanel();
-        JButton button1 = new JButton("Aceptat");
+        JButton button1 = new JButton("Aceptar");
         panel.add(label);
         panel.add(comboBox);
         panel.add(button1);
@@ -36,34 +37,63 @@ public class interfaceUs extends JFrame implements ActionListener{
 
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Abre la ventana para introducir el número
-                JFrame introducirNumeroFrame = new JFrame("Introducir número");
-                JLabel introducirNumeroLabel = new JLabel("Introduce un número:");
-                JTextField introducirNumeroTextField = new JTextField(10);
-                JButton introducirNumeroAceptarButton = new JButton("Aceptar");
-                JPanel introducirNumeroPanel = new JPanel();
-                introducirNumeroPanel.add(introducirNumeroLabel);
-                introducirNumeroPanel.add(introducirNumeroTextField);
-                introducirNumeroPanel.add(introducirNumeroAceptarButton);
-                introducirNumeroFrame.add(introducirNumeroPanel);
-                introducirNumeroFrame.pack();
-                introducirNumeroFrame.setLocationRelativeTo(null);
-                introducirNumeroFrame.setVisible(true);
+                int opcionSeleccionada = comboBox.getSelectedIndex();
 
-                // Agrega un ActionListener al botón "Aceptar" de la ventana para introducir el número
-                introducirNumeroAceptarButton.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        // Cierra la ventana para introducir el número
-                        introducirNumeroFrame.dispose();
+                if (opcionSeleccionada == 0) {
+                    ventanaCorrespondiente = new JFrame("Conversor de moneda");
+                    JTextField textField = new JTextField(10);
+                    JPanel panel = new JPanel();
+                    panel.add(new JLabel("Introduce un número:"));
+                    panel.add(textField);
+                    JButton okButton = new JButton("OK");
+                    okButton.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            ventanaCorrespondiente.dispose();
+                            String texto = textField.getText();
+                            double numero = Double.parseDouble(texto);
+                            double resultado = numero * 1.23; // Conversión a dólares
+                            JOptionPane.showMessageDialog(null, numero + " euros son " + resultado + " dólares.");
+                        }
+                    });
+                    JButton cancelButton = new JButton("Cancelar");
+                    cancelButton.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            ventanaCorrespondiente.dispose();
+                        }
+                    });
+                    panel.add(okButton);
+                    panel.add(cancelButton);
+                    ventanaCorrespondiente.add(panel);
+                } else if (opcionSeleccionada == 1) {
+                    ventanaCorrespondiente = new JFrame("Conversor de Temperatura");
+                    JTextField textField = new JTextField(10);
+                    JPanel panel = new JPanel();
+                    panel.add(new JLabel("Introduce un número:"));
+                    panel.add(textField);
+                    JButton okButton = new JButton("OK");
+                    okButton.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            ventanaCorrespondiente.dispose();
+                            String texto = textField.getText();
+                            double numero = Double.parseDouble(texto);
+                            double resultado = numero * 1.23; // Conversión a dólares
+                            JOptionPane.showMessageDialog(null, numero + " euros son " + resultado + " dólares.");
+                        }
+                    });
+                    JButton cancelButton = new JButton("Cancelar");
+                    cancelButton.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            ventanaCorrespondiente.dispose();
+                        }
+                    });
+                    panel.add(okButton);
+                    panel.add(cancelButton);
+                    ventanaCorrespondiente.add(panel);
+                }
 
-                        // Abre la nueva ventana con las opciones adicionales
-                        JFrame nuevaVentanaFrame = new JFrame("Nueva ventana");
-                        nuevaVentanaFrame.setSize(400, 300);
-                        nuevaVentanaFrame.setLocationRelativeTo(null);
-                        // Aquí agrega los componentes necesarios para la nueva ventana
-                        nuevaVentanaFrame.setVisible(true);
-                    }
-                });
+                ventanaCorrespondiente.setSize(400, 300);
+                ventanaCorrespondiente.setLocationRelativeTo(null);
+                ventanaCorrespondiente.setVisible(true);
             }
         });
     }
